39c79aafb336a557be787564881939b02a7abfe7
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-09-29 09:26:23 +0700

    Update duan.sql

--- SQL/duan.sql
+++ SQL/duan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 26, 2022 at 10:36 AM
+-- Generation Time: Sep 29, 2022 at 02:25 AM
 -- Server version: 10.9.3-MariaDB-log
 -- PHP Version: 8.1.6
 
@@ -32,6 +32,18 @@ CREATE TABLE `brand_categrory` (
   `idCate` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `brand_categrory`
+--
+
+INSERT INTO `brand_categrory` (`idBrand`, `idCate`) VALUES
+(1, 1),
+(1, 2),
+(3, 1),
+(3, 2),
+(13, 1),
+(13, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -51,7 +63,8 @@ CREATE TABLE `categrory` (
 --
 
 INSERT INTO `categrory` (`idcate`, `cateName`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Áo Polo', 1, '2022-09-26 03:44:39', '2022-09-26 08:12:58');
+(1, 'Giày', 0, NULL, NULL),
+(2, 'Quần áo', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -66,6 +79,30 @@ CREATE TABLE `colors` (
   `status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `colors`
+--
+
+INSERT INTO `colors` (`id`, `colorName`, `colorpicker`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Đen', '#000000', 1, '2022-09-28 10:45:31', NULL),
+(2, 'Đỏ', '#ff0000', 1, '2022-09-28 10:45:46', '2022-09-28 10:54:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -87,15 +124,41 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2022_09_16_044254_user_role', 1),
 (2, '2014_10_12_000000_create_users_table', 2),
-(3, '2022_09_24_094315_categrories', 3),
-(4, '2022_09_24_094950_tbl_tag', 4),
-(6, '2022_09_13_031313_products', 6),
-(8, '2022_09_13_032157_color_bar', 8),
-(9, '2022_09_13_032903_storages', 9),
-(10, '2022_09_25_005701_prod_tag', 10),
-(12, '2022_09_24_101406_tbl_size', 12),
-(13, '2022_09_25_013933_storage_prod', 13),
-(14, '2022_09_25_015538_brand_tbl', 14);
+(15, '2014_10_12_100000_create_password_resets_table', 3),
+(16, '2019_08_19_000000_create_failed_jobs_table', 3),
+(17, '2019_12_14_000001_create_personal_access_tokens_table', 3),
+(18, '2022_09_26_095210_create_brand_category_table', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -181,7 +244,8 @@ CREATE TABLE `tbl_brand` (
 
 INSERT INTO `tbl_brand` (`idbrand`, `brandname`, `status`, `created_at`, `updated_at`) VALUES
 (1, 'Nike', 0, '2022-09-25 06:12:14', '2022-09-25 06:12:14'),
-(3, 'Adidas', 0, '2022-09-25 07:53:45', '2022-09-25 08:54:54');
+(3, 'Adidas', 0, '2022-09-25 07:53:45', '2022-09-25 08:54:54'),
+(13, 'Gucci', 0, '2022-09-26 12:04:52', '2022-09-26 12:04:52');
 
 -- --------------------------------------------------------
 
@@ -230,8 +294,8 @@ CREATE TABLE `userrole` (
 --
 
 INSERT INTO `userrole` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 1, '2022-09-17 11:01:08', NULL),
-(2, 'Nhân viên', 1, '2022-09-17 11:20:22', NULL);
+(1, 'admin', 1, '2022-09-18 14:53:08', NULL),
+(2, 'user', 1, '2022-09-19 04:39:01', NULL);
 
 -- --------------------------------------------------------
 
@@ -261,8 +325,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `password2`, `fullName`, `image`, `email`, `email_verified_at`, `ggId`, `idRole`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$8VyuTdggxGSuHHmpbutwkOhe0B3cvYvVq/ls2It5ueV6dgSsqBxHS', NULL, NULL, NULL, 'leodomsolar@gmail.com', NULL, NULL, 1, 1, NULL, '2022-09-17 11:22:33', NULL),
-(3, 'user1', '$2y$10$0r.43da.NOJAY./5dLeYu.Hp68XmJugFK7Ik0abCFvmr4OuCzvxiW', NULL, NULL, NULL, 'trungthanh01233@gmail.com', NULL, NULL, 2, 1, NULL, '2022-09-26 06:21:31', NULL);
+(1, 'admin', '$2y$10$8VyuTdggxGSuHHmpbutwkOhe0B3cvYvVq/ls2It5ueV6dgSsqBxHS', NULL, NULL, NULL, 'leodomsolar@gmail.com', NULL, NULL, 1, 1, NULL, '2022-09-17 11:22:33', NULL);
 
 --
 -- Indexes for dumped tables
@@ -288,10 +351,31 @@ ALTER TABLE `colors`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
 -- Indexes for table `prodtage`
@@ -313,6 +397,7 @@ ALTER TABLE `products`
 -- Indexes for table `storageprod`
 --
 ALTER TABLE `storageprod`
+  ADD PRIMARY KEY (`idProd`,`idSize`,`idColor`,`idStorage`),
   ADD KEY `storageprod_idprod_foreign` (`idProd`),
   ADD KEY `storageprod_idsize_foreign` (`idSize`),
   ADD KEY `storageprod_idcolor_foreign` (`idColor`),
@@ -370,13 +455,25 @@ ALTER TABLE `categrory`
 -- AUTO_INCREMENT for table `colors`
 --
 ALTER TABLE `colors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `prodtage`
@@ -400,7 +497,7 @@ ALTER TABLE `storages`
 -- AUTO_INCREMENT for table `tbl_brand`
 --
 ALTER TABLE `tbl_brand`
-  MODIFY `idbrand` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idbrand` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tbl_size`
